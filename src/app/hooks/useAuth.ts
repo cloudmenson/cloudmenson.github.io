5da310d76b99e781924c5aaa83e49dc8b793f460
Danylo <cloudmenson9@gmail.com>
2025-04-08 16:10:19 +0300

    duplicate logic

--- src/app/hooks/useAuth.ts
+++ src/app/hooks/useAuth.ts
@@ -4,33 +4,22 @@ import { useRouter } from "next/navigation";
 import { signInWithPopup } from "firebase/auth";
 
 import { auth, googleProvider } from "@/app/lib/firebase";
-import { useAuthStore } from "../store/modalStore";
 
 export const useFirebaseLogin = () => {
   const router = useRouter();
 
-  const { setUser, user } = useAuthStore();
-
   const signInWithGoogle = async () => {
     try {
-      const result = await signInWithPopup(auth, googleProvider);
-      console.log(result.user.email);
-
-      const userData = {
-        uid: result.user.uid,
-        displayName: result.user.displayName,
-        email: result.user.email,
-        photoURL: result.user.photoURL,
-      };
-
-      setUser(userData);
-
-      console.log("Успішний вхід:", user);
+      await signInWithPopup(auth, googleProvider);
 
       // тут можна записати user в стор, cookies або context
       router.push("/userpage");
-    } catch (err: any) {
-      console.error("Помилка входу:", err.message);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        console.error("Error:", err.message);
+      } else {
+        console.error("Unknown error", err);
+      }
     }
   };
 
